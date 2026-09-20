11cab898bb512a2a481524ae4ac45265c1e25221
Zahid Extra <xahidprime9@gmail.com>
2026-09-20 14:31:27 +0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const settings = {
-  packname: '*~_ 𝐗𝐭𝐲𝐥𝐢𝐬𝐡_ღ꙰𝐒𝐡𝐚𝐡𝐢𝐧࿐_~*',
+  packname: '*~— 𝚳 𝚨 Ꭱ 𝐂 𝚶 ⤹ ☕*',
   author: '‎',
-  botName: "*~_ 𝐗𝐭𝐲𝐥𝐢𝐬𝐡_ღ꙰𝐒𝐡𝐚𝐡𝐢𝐧࿐_~*",
-  botOwner: '*~_𖣘𝐒𝐡𝐚𝐡𝐢𝐧 𝐑𝐚𝐧𝐚𖣘_~*', // Your name
+  botName: "*— 𝚳 𝚨 Ꭱ 𝐂 𝚶 ⤹ ☕*",
+  botOwner: '*— 𝚳 𝚨 Ꭱ 𝐂 𝚶 ⤹ ☕*', // Your name
   ownerNumber: '8801793447339', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
